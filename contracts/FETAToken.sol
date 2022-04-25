0b6ad46be770ee699a46375deb181992d9733650
Ryan Shtirmer <rshtirmer@gmail.com>
2022-04-24 23:54:29 -0400

    Update and rename XYZToken.sol to FETAToken.sol

--- contracts/FETAToken.sol
+++ contracts/FETAToken.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract XYZToken is ERC20Burnable, Ownable {
-    uint256 private constant SUPPLY = 1_000_000_000 * 10**18;
+contract FETAToken is ERC20Burnable, Ownable {
+    uint256 private constant SUPPLY = 100_000_000 * 10**18;
 
-    constructor() public ERC20("XYZ Governance Token", "XYZ") {
+    constructor() public ERC20("FETA Governance Token", "FETA") {
         _mint(msg.sender, SUPPLY);
     }
 
